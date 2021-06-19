6e5dbfb1338fbdd95c53562feda356df399710b6
gabrielbur <gabriel.bur@outlook.com>
2021-06-19 15:36:13 -0300

    first

---
+++ Frotcom.Challenge.SendTrackingDataWorker/Program.cs
@@ -0,0 +1,22 @@
+﻿using Frotcom.Challenge.Data.Models;
+using Frotcom.Challenge.Queue;
+using Frotcom.Challenge.Reverse.Geocoding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Frotcom.Challenge.SendTrackingDataWorker
+{
+    class Program
+    {
+        /// <summary>
+        /// FROTCOM CHALLENGE STARTS HERE
+        /// </summary>
+        public static void Main()
+        {
+            //TODO
+        }
+    }
+}
